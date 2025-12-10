90f699c5b392f0a22e7084c6362c454fbcbfd417
ethan875 <140220879+ethan875@users.noreply.github.com>
2025-12-10 13:47:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.c7zhwlkrpgi0.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "chuntian123"
+custompass = "YES"
 customdb = "employee"
 custombucket = "chutian-employee"
 customregion = "us-east-1"
